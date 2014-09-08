f583446dd9a0c6292b84392baed6f8c6b985bf59
James Gray <jgray@atlassian.com>
2014-09-08 19:02:20 +1000

    Modified Google Analytics to use new 'analytics.js'

--- conf.py
+++ conf.py
@@ -666,28 +666,48 @@ SEARCH_FORM = """
 # Extra things you want in the pages HEAD tag. This will be added right
 # before </head>
 # (translatable)
-# EXTRA_HEAD_DATA = ""
+EXTRA_HEAD_DATA = """
+<!--Google Analytics-->
+<script>
+(function(i, s, o, g, r, a, m){
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function(){
+        (i[r].q = i[r].q||[]).push(arguments)
+    },
+    i[r].l = 1*new Date();
+    a = s.createElement(o),
+    m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a,m)
+})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+ga('create', 'UA-28692595-1', 'auto');
+ga('set', 'forceSSL', true);        // Send all data using SSL, even from insecure (HTTP) pages.
+ga('send', 'pageview');
+</script>
+"""
 # Google Analytics or whatever else you use. Added to the bottom of <body>
 # in the default template (base.tmpl).
 # (translatable)
 BODY_END = """
 <!--Google Analytics-->
-<script type="text/javascript">
-    var _gaq = _gaq || [];
-        _gaq.push(['_setAccount', 'UA-28692595-1']);
-        _gaq.push(['_setDomainName', 'gray.net.au']);
-        _gaq.push(['_trackPageview']);
-
-    (function() {
-        var ga = document.createElement('script');
-        ga.type = 'text/javascript';
-        ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0];
-        s.parentNode.insertBefore(ga, s);
-    })
-    ();
-</script>
+//<script type="text/javascript">
+//    var _gaq = _gaq || [];
+//        _gaq.push(['_setAccount', 'UA-28692595-1']);
+//        _gaq.push(['_setDomainName', 'gray.net.au']);
+//        _gaq.push(['_trackPageview']);
+//
+//    (function() {
+//        var ga = document.createElement('script');
+//        ga.type = 'text/javascript';
+//        ga.async = true;
+//        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+//        var s = document.getElementsByTagName('script')[0];
+//        s.parentNode.insertBefore(ga, s);
+//    })
+//    ();
+//</script>
 """
 
 # The possibility to extract metadata from the filename by using a
